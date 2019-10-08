1696949c3326056ea2b2b8f2d396ed3024896e09
Andrew Capp <acapp909@gmail.com>
2019-10-08 22:58:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  my_array = ["dogs", "cats", "birds"]
+  my_array = ["dogs", "cats", "birds", "chickens"]
 end
 
 def add_element_to_end_of_array(array, element)
